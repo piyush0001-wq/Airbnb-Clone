e699b5a587d6573153e6e944cf82a371354c96f4
piyush solakhiya <64743126+piyush0001-wq@users.noreply.github.com>
2021-05-06 11:16:53 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDJXeyp0H7ZbblmTC1WfWpyHlOMRcyRHHo",
-  authDomain: "airbnb-clone-a24f8.firebaseapp.com",
-  projectId: "airbnb-clone-a24f8",
-  storageBucket: "airbnb-clone-a24f8.appspot.com",
-  messagingSenderId: "90211289944",
-  appId: "1:90211289944:web:903f0dbd08cff3d2fbf32d"
+//your firebase api//
 });
 
 const db = firebaseApp.firestore();
